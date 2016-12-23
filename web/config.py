0cab59eb401ea97e8082e06720f8fb547e56dfb8
Your Name <you@example.com>
2016-12-23 12:51:46 +0100

    Archi

---
+++ web/config.py
@@ -0,0 +1,2 @@
+# IGN
+ign_api_key = '0ldfl0m1mud840h6oggz9y95'
---
+++ web/site.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from flask import Flask, render_template, request, redirect
+# from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+
+app = Flask(__name__)  # Convention Flask
+
+PROJECT_DIR = Path(__file__).absolute().parent
+VAR_DIR = PROJECT_DIR.parent / 'var'
+DB_PATH = VAR_DIR / 'db.sqlite'
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + str(DB_PATH)
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+
+
+# class Contact(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     nom = db.Column(db.String())
+#     prenom = db.Column(db.String())
+#     telephone = db.Column(db.String())
+
+
+# db.create_all()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', ign_api_key=config.ign_api_key)
+
+
+
+@app.route('/contact/create', methods=['POST'])
+def create_contact():
+    new_contact = Contact(
+        nom=request.form['nom'].upper(),
+        prenom=request.form['prenom'].title(),
+        telephone=request.form['telephone'])
+    db.session.add(new_contact)
+    db.session.commit()
+    return redirect('/contact/' + str(new_contact.id))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
